9acbd11d17e0c8c0e34164b85e0465f9177bc830
Elias Schepis <schepiselias@gmail.com>
2025-07-21 19:10:54 +0200

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,8 @@
+class Config:
+    BASE_URL = "http://zero.webappsecurity.com/"
+    INDEX_URL = BASE_URL + "index.html"
+    LOGIN_URL = BASE_URL + "login.html"
+    USERNAME = "username"
+    PASSWORD = "password"
+    WRONG_USER = "usuario_invalido"
+    WRONG_PASSWORD = "clave_invalida"
